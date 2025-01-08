433c6cf310863067144d45132494b8397a6ded17
evgen190 <sokolevgeniy239416@gmail.com>
2025-01-08 21:41:23 +0500

    edit css

--- js/app.js
+++ js/app.js
@@ -49,6 +49,11 @@ function addImportant() {
     })
 }
 
+function addEdit() {
+    const editButtons = document.querySelectorAll('.edit')
+
+}
+
 function displayMessages() {
     if (!todoList.length) {
         todo.innerHTML = 'No tasks'
@@ -63,44 +68,16 @@ function displayMessages() {
                 <label for='item${i}' class='${item.important ? 'important' : ''}'>
                 ${item.todo}</label>
 
-                <div>
+                <div class="tools">
+                    <img class='edit' data-index='${i}' src='./icons/edit.png' alt='edit'>
                     <img class='important-btn' data-index='${i}' src='./icons/important.png' alt='important'>
                     <img class='delete' data-index='${i}' src='./icons/delete.png' alt='delete'>
                 </div>
             </li>
         `
-
-        todo.innerHTML = displayMessage
-        delTask()   
-        addImportant()
     })
+    todo.innerHTML = displayMessage
+    delTask()   
+    addImportant()
 }
 
-todo.addEventListener('change', (event) =>{
-    let valueLabel = todo.querySelector(
-        '[for=' + event.target.getAtttribute('id') + ']'
-    ).innerHTML
-
-    todoList.forEach((item, i) => {
-        if (item.todo === valueLabel) {
-            item.checked = !item.checked
-            localStorage.setItem('todo', JSON.stringify(todoList))
-        }
-    })
-})
-
-todo.addEventListener('contextmenu', (event) => {
-    event.preventDefault()
-    todoList.forEach((item, i) => {
-        if (item.todo === event.target.innerHTML) {
-            if (event.todo === event.target.innerHTML) {
-                todoList.slice(i, 1)
-            } else {
-                item.important = !item.important
-            }
-
-            displayMessages()
-            localStorage.setItem('todo', JSON.stringify(todoList))
-        }
-    })
-})
